582336dc78ad0eb87e968b0dfa0dcde390f830e9
James Underwood <ju003213@ohio.edu>
2016-11-28 22:05:35 -0500

    Cleaned up most of the unnecessary ASM directives, except for .text. I think in the future we'll need to create our own spec in ./lib/MC/MCParser/ or build our own assembler in LLVM.
    Addition: Adjusted typo

--- lib/Target/NOPE/MCTargetDesc/NOPEMCAsmInfo.cpp
+++ lib/Target/NOPE/MCTargetDesc/NOPEMCAsmInfo.cpp
@@ -17,10 +17,38 @@ using namespace llvm;
 void NOPEMCAsmInfo::anchor() { }
 
 NOPEMCAsmInfo::NOPEMCAsmInfo(const Triple &TT) {
-  PointerSize = CalleeSaveStackSlotSize = 2;
 
+  // Note that the MPASM assembler does not accept any `.section` headings
+  // We took pains to get around or delete all of the headings
+  // TODO: Implement `banksel` `pagesel` and such.
+  // ALSO TODO: For readability is `equ` e.g. `NAME equ ADDR` possible?
+
+  PointerSize = 1;
+  CalleeSaveStackSlotSize = 1; // This will most likely change
+                               // as functions are implemented.
+
+  MaxInstLength = 1;
+  SeparatorString = "\n";      // All instructions on separate lines
   CommentString = ";";
+  GlobalDirective = "    ; .globl  ";
 
-  AlignmentIsInBytes = false;
+  HasFunctionAlignment = false;
+  HasDotTypeDotSizeDirective = false;
+  HasSingleParameterDotFile = false;
+  HasIdentDirective = false;
+
+  HiddenVisibilityAttr = MCSA_Invalid;
+  HiddenDeclarationVisibilityAttr = MCSA_Invalid;
+  ProtectedVisibilityAttr = MCSA_Invalid;
+  
+  SupportsDebugInformation = false;      // Debugging not yet implemented
+                                         // Implemented in assembler itself?
+
+  UseIntegratedAssembler = false;        // Using external assembler
+
+  AlignmentIsInBytes = false;            // MSP430 old code. TODO: Prune
   UsesELFSectionDirectiveForBSS = true;
+
+  // From MCAsmInfoELF.h
+  UsesNonexecutableStackSection = false;
 }
