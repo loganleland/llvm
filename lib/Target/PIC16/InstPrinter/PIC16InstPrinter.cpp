9d7b6e36006f42297d25ad98e3f9900c1b527f2d
Logan Leland <ll734713@ohio.edu>
2017-01-30 22:41:37 -0500

    can print negative operands now, conversion between base 10 and base 16 now

--- lib/Target/PIC16/InstPrinter/PIC16InstPrinter.cpp
+++ lib/Target/PIC16/InstPrinter/PIC16InstPrinter.cpp
@@ -38,52 +38,68 @@ void PIC16InstPrinter::printPCRelImmOperand(const MCInst *MI, unsigned OpNo,
                                              raw_ostream &O) {
   const MCOperand &Op = MI->getOperand(OpNo);
   stringstream OPbase16;
-  if (Op.isImm()) {
-    OPbase16 << hex << Op.getImm();
-    O << OPbase16.str(); 
-    OPbase16.str("");
+  int alpha;
+  if(Op.isImm()){
+    if(Op.getImm() < 0) {
+      alpha=Op.getImm()-Op.getImm()-Op.getImm();
+      OPbase16 << hex << alpha;
+      O << '-' << "0x" << OPbase16.str(); 
+    }
+    else if(Op.getImm() > 0) {
+      OPbase16 << hex << Op.getImm();
+      O << "0x" << OPbase16.str(); 
+    }
   }
   else {
     assert(Op.isExpr() && "unknown pcrel immediate operand");
     Op.getExpr()->print(O, &MAI);
   }
+  OPbase16.str("");
 }
 
 void PIC16InstPrinter::printOperand(const MCInst *MI, unsigned OpNo,
                                      raw_ostream &O, const char *Modifier) {
   assert((Modifier == nullptr || Modifier[0] == 0) && "No modifiers supported");
+
   const MCOperand &Op = MI->getOperand(OpNo);
+  int alpha;
   stringstream OPbase16;
+
   if (Op.isReg()) {
     O << getRegisterName(Op.getReg());
-  } else if (Op.isImm()) {
-      //Imm is in base 10 but our assembler prefers base16
-      //d'' doesn't work due to calculating the stack/frame
-      // pointer offset
-      OPbase16 << hex << Op.getImm();
-      O << OPbase16.str(); 
-      OPbase16.str("");
-  } else {
-      assert(Op.isExpr() && "unknown operand kind in printOperand");
-      Op.getExpr()->print(O, &MAI);
   }
+  else if (Op.isImm() and Op.getImm()<0) {
+    alpha=Op.getImm()-Op.getImm()-Op.getImm();
+    OPbase16 << hex << alpha;
+    O << '-' << "0x" << OPbase16.str(); 
+  }
+  else if (Op.isImm() and Op.getImm()>=0) {
+    OPbase16 << hex << Op.getImm();
+    O << "0x" << OPbase16.str(); 
+  }   
+
+  else {
+    assert(Op.isExpr() && "unknown operand kind in printOperand");
+    Op.getExpr()->print(O, &MAI);
+  }
+  OPbase16.str("");
 }
 
 void PIC16InstPrinter::printSrcMemOperand(const MCInst *MI, unsigned OpNo,
                                            raw_ostream &O,
                                            const char *Modifier) {
   const MCOperand &Disp = MI->getOperand(OpNo+1);
-  uint64_t alpha = Disp.getImm()-Disp.getImm()-Disp.getImm();
+  uint64_t alpha = Disp.getImm();
   stringstream OPbase16;
   // Print displacement first
-
   if (Disp.isExpr())
     Disp.getExpr()->print(O, &MAI);
   else {
     assert(Disp.isImm() && "Expected immediate in displacement field");
+    alpha=alpha-alpha-alpha;
     OPbase16 << hex << alpha;
-    O << OPbase16.str(); 
-    OPbase16.str("");
+    O << "0x" << OPbase16.str(); 
+    OPbase16.str("");  
   }
 }
 
