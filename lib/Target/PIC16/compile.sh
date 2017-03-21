d8defda47e38d4bc1d3a5dec43ec07be0bb835de
rnewlun <rnewlun@gmail.com>
2017-03-20 20:14:46 -0400

    Update compilation script with an output log file to push to server later

--- lib/Target/PIC16/compile.sh
+++ lib/Target/PIC16/compile.sh
@@ -4,16 +4,20 @@
 # file for the compiler. It should be in the format of C. The output of
 # the compiler will be the input file with the extension changed to .asm
 
-# run clang targeting pic16 on input C file
+# Create timestamp variable
+DATE=$(date +%F_%H_%M_%S)
+echo $DATE
+
+# Run clang targeting pic16 on input C file
 ./bin/clang -S -emit-llvm -target pic16 $1
 
-# run llc targeting pic16 on LLVM IR file generated from clang
-./bin/llc -mcpu=generic -march=pic16 ${1%.c}.ll
+# Run llc targeting pic16 on LLVM IR file generated from clang
+./bin/llc -mcpu=generic -march=pic16 --stats ${1%.c}.ll 2> output.log
+mv ./output.log ./$DATE.log
 
-# run the post processing script to tidy up the assembly .s file and rename
+# Run the post processing script to tidy up the assembly .s file and rename
 # it to a .asm assembly file
 ./postLLC.sh ${1%.c}.s
 
-# open to survey
-sleep 3
+# Open to survey
 xdg-open https://goo.gl/forms/AYxzRiEtKxIy9aBy2
